3ecc9ff0829ba773d06f55ef97c85f49f1acaec3
FiveMountainTomoto <2257485855@qq.com>
2025-07-09 14:53:16 +0800

    (+)Coordinate

---
+++ geodesy-data-struct/DataClass/Coordinate/GeodeticCoordinate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geodesy_data_struct.DataClass.Coordinate
+{
+    public class GeodeticCoordinate
+    {
+        public required Ellipsoid CoordinateSystem { get; init; }
+        public required Angle Lat { get; init; }
+        public required Angle Lon { get; init; }
+        public required double H { get; set; }
+        public Angle B => Lat;
+        public Angle L => Lon;
+    }
+}
---
+++ geodesy-data-struct/DataClass/Coordinate/SpaceRectangularCoordinate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geodesy_data_struct.DataClass.Coordinate
+{
+    public class SpaceRectangularCoordinate
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+    }
+}
