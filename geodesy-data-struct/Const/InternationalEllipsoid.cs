68bb4226010098f1ed4269b48b8b01ec1b417eba
FiveMountainTomoto <2257485855@qq.com>
2025-07-09 14:48:15 +0800

    (+)Ellipsoid

---
+++ geodesy-data-struct/Const/InternationalEllipsoid.cs
@@ -0,0 +1,31 @@
+﻿using geodesy_data_struct.DataClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geodesy_data_struct.Const
+{
+    public static class InternationalEllipsoid
+    {
+        public static readonly Ellipsoid WGS84 = new()
+        {
+            Name = "WGS84",
+            SemiMajorAxisLength = 6378137,
+            SemiMinorAxisLength = 6356752.3142,
+            Flattening = 1 / 298.257223563,
+            FirstEccentricitySquared = 6.69437999013e-3,
+            SecondEccentricitySquared = 6.73949674227e-3
+        };
+        public static readonly Ellipsoid CGCS2000 = new()
+        {
+            Name = "CGCS2000",
+            SemiMajorAxisLength = 6378137,
+            SemiMinorAxisLength = 6356752.3141,
+            Flattening = 1 / 298.257222101,
+            FirstEccentricitySquared = 6.69438002290e-3,
+            SecondEccentricitySquared = 6.73949677548e-3
+        };
+    }
+}
---
+++ geodesy-data-struct/DataClass/Ellipsoid.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geodesy_data_struct.DataClass
+{
+    public class Ellipsoid
+    {
+        public required string Name { get; init; }
+        public double SemiMajorAxisLength { get; init; }
+        public double SemiMinorAxisLength { get; init; }
+        public double Flattening { get; init; }
+
+        private double _firstEccentricitySquared = 0;
+        public double FirstEccentricitySquared
+        {
+            get
+            {
+                return _firstEccentricitySquared == 0 ? (A * A - B * B) / (A * A) : _firstEccentricitySquared;
+            }
+            init
+            {
+                _firstEccentricitySquared = value;
+            }
+        }
+
+        private double _secondEccentricitySquared = 0;
+        public double SecondEccentricitySquared
+        {
+            get
+            {
+                return _secondEccentricitySquared == 0 ? (A * A - B * B) / (A * A) : _secondEccentricitySquared;
+            }
+            init
+            {
+                _secondEccentricitySquared = value;
+            }
+        }
+
+        public double A => SemiMajorAxisLength;
+        public double B => SemiMinorAxisLength;
+        public double F => Flattening;
+        public double E12 => FirstEccentricitySquared;
+        public double E22 => SecondEccentricitySquared;
+    }
+}
